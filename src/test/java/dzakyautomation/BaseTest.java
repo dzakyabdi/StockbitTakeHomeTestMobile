d1da83b7ef9b5d7966a785b41c3a7348867cfc3b
dzaky <dzakyabdi17@gmail.com>
2025-10-26 17:54:26 +0700

    first commit

---
+++ src/test/java/dzakyautomation/BaseTest.java
@@ -0,0 +1,41 @@
+package dzakyautomation;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+    AndroidDriver driver;
+    AppiumDriverLocalService service;
+
+    @BeforeClass
+    public void ConfigureAppium() throws MalformedURLException {
+        service = new AppiumServiceBuilder()
+                .withAppiumJS(new File("C:\\Users\\dzaky\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js"))
+                .withIPAddress("127.0.0.1").usingPort(4723).build();
+        service.start();
+
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setDeviceName("Pixel_3a");
+//        options.setApp("D:\\Automation Blibli\\appium_1\\src\\test\\rscrs\\ApiDemos-debug.apk");
+        options.setApp("D:\\Automation Blibli\\appium_1\\src\\test\\rscrs\\mda-1.0.13-15.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+        service.stop();
+    }
+}
---
+++ src/test/java/dzakyautomation/StockbitMobileTest.java
@@ -0,0 +1,53 @@
+package dzakyautomation;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class StockbitMobileTest extends BaseTest {
+    @Test
+    public void BuyProductTest() throws InterruptedException {
+        System.out.println("Appium Basics");
+
+        // Actual Automation
+        driver.findElement(By.xpath("//androidx.recyclerview.widget.RecyclerView[@content-desc='Displays all products of catalog']/android.view.ViewGroup/android.widget.ImageView[@content-desc='Sauce Lab Back Packs']")).click();
+        driver.findElement(By.xpath("//android.widget.ImageView[@content-desc='Blue color']")).click();
+
+        driver.findElement(By.xpath("//android.widget.ImageView[@content-desc='Increase item quantity']")).click();
+        String quantity = driver.findElement(By.xpath("//android.widget.TextView[@resource-id='com.saucelabs.mydemoapp.android:id/noTV']")).getText();
+        System.out.println("Current Quantity: " + quantity);
+
+        driver.findElement(By.xpath("//android.widget.Button[@content-desc='Tap to add product to cart']")).click();
+        driver.findElement(By.xpath("//android.widget.RelativeLayout[@content-desc='View cart']")).click();
+
+        driver.findElement(By.xpath("//android.widget.Button[@content-desc='Confirms products for checkout']")).click();
+
+        //Input username password
+        driver.findElement(By.xpath("//android.widget.EditText[@resource-id='com.saucelabs.mydemoapp.android:id/nameET']")).sendKeys("automationtesting");
+        driver.findElement(By.xpath("//android.widget.EditText[@resource-id='com.saucelabs.mydemoapp.android:id/passwordET']")).sendKeys("Password123");
+        driver.findElement(By.xpath("//android.widget.LinearLayout[@resource-id='com.saucelabs.mydemoapp.android:id/buttonLL']")).click();
+
+        //Input Shipping Address
+        driver.findElement(By.xpath("//android.widget.EditText[@resource-id='com.saucelabs.mydemoapp.android:id/fullNameET']")).sendKeys("Automation Testing");
+        driver.findElement(By.xpath("//android.widget.EditText[@resource-id='com.saucelabs.mydemoapp.android:id/address1ET']")).sendKeys("Automation Address 1");
+        driver.findElement(By.xpath("//android.widget.EditText[@resource-id='com.saucelabs.mydemoapp.android:id/address2ET']")).sendKeys("Automation Address 2");
+        driver.findElement(By.xpath("//android.widget.EditText[@resource-id='com.saucelabs.mydemoapp.android:id/cityET']")).sendKeys("Jakarta");
+        driver.findElement(By.xpath("//android.widget.EditText[@resource-id='com.saucelabs.mydemoapp.android:id/stateET']")).sendKeys("Jakarta Timur");
+        driver.findElement(By.xpath("//android.widget.EditText[@resource-id='com.saucelabs.mydemoapp.android:id/zipET']")).sendKeys("89750");
+        driver.findElement(By.xpath("//android.widget.EditText[@resource-id='com.saucelabs.mydemoapp.android:id/countryET']")).sendKeys("Indonesia");
+        driver.findElement(By.id("com.saucelabs.mydemoapp.android:id/paymentBtn")).click();
+
+        //Input Payment Method
+        driver.findElement(By.xpath("//android.widget.EditText[@resource-id='com.saucelabs.mydemoapp.android:id/nameET']")).sendKeys("Automation Payment");
+        driver.findElement(By.xpath("//android.widget.EditText[@resource-id='com.saucelabs.mydemoapp.android:id/cardNumberET']")).sendKeys("3258 1256 7568 7891");
+        driver.findElement(By.xpath("//android.widget.EditText[@resource-id='com.saucelabs.mydemoapp.android:id/expirationDateET']")).sendKeys("03/25");
+        driver.findElement(By.xpath("//android.widget.EditText[@resource-id='com.saucelabs.mydemoapp.android:id/securityCodeET']")).sendKeys("12345");
+
+        driver.findElement(By.id("com.saucelabs.mydemoapp.android:id/paymentBtn")).click();
+
+        //Review Order
+        driver.findElement(By.id("com.saucelabs.mydemoapp.android:id/paymentBtn")).click();
+
+        Thread.sleep(5000);
+        tearDown();
+    }
+}
